d1a748aeef063899ddc1897af92e495c6d6fe58b
jeo19 <terrakkk@outlook.com>
2018-08-26 00:33:11 +0630

    injectGlobal and extend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component, Fragment } from 'react';
-import styled from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
+injectGlobal`
+body{
+  padding:0px
+  margin:0px
+}
+`;
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Container>
         <Button />
         <Button danger />
-      </Fragment>
+        <Anchor href="http://google.com">Go to google.com</Anchor>
+      </Container>
     );
   }
 }
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
 const Button = styled.button`
   border-radius: 20px;
   padding: 5px;
@@ -23,5 +35,8 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${props => (props.danger ? '#e67e22' : '#2ecc71')};
+`;
+const Anchor = Button.withComponent('a').extend`
+text-decoration:none
 `;
 export default App;
