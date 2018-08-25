95358a7a7588f84b6b483ea4aed536449415cb0d
jeo19 <terrakkk@outlook.com>
2018-08-26 00:04:22 +0630

    yarn add StyledComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import styled from 'styled-components';
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Fragment>
+        <Button />
+        <Button danger />
+      </Fragment>
+    );
   }
 }
-
+const Button = styled.button`
+  border-radius: 20px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  border: block;
+  font: weight 600;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? '#e67e22' : '#2ecc71')};
+`;
 export default App;
